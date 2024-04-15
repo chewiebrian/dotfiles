fb6859efa5a2e81b8f256b33aa4dc51bfafeb451
fernando.gil <fernando.gil@buntplanet.com>
2024-04-15 16:27:08 +0200

    Improve ssm_connect function to detect tokens already in use

--- fish/.config/fish/functions/ssm_connect.fish
+++ fish/.config/fish/functions/ssm_connect.fish
@@ -1,4 +1,10 @@
 function ssm_connect
+        if not test -z "(crudini --get ~/.aws/credentials | grep arn:aws:sts)"
+          set AWS_PROFILE (crudini --get ~/.aws/credentials | grep arn:aws:sts)
+          export AWS_PROFILE=$AWS_PROFILE
+          export AWS_DEFAULT_REGION=eu-central-1
+          export AWS_DEFAULT_REGION=(aws --profile "$AWS_PROFILE" ec2 describe-regions | jq -r .Regions[].RegionName | fzf -e --height 1% --header 'Select AWS region' --reverse --info=hidden --query $AWS_DEFAULT_REGION --preview 'aws --profile='$AWS_PROFILE' ssm get-parameter --name /aws/service/global-infrastructure/regions/{}/longName | jq -r .Parameter.Value')
+        end
         if test -z "$AWS_PROFILE"          
           set AWS_PROFILE (cat ~/.aws/config | grep -Po "(?<=\\[).*(?=\\])" | grep "profile" | cut -f2 -d' ' | fzf -e --height 1% --header 'Select AWS profile' --reverse --info=hidden)
           test -z "$AWS_PROFILE" && return
