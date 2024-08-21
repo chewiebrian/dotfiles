d8032bcd4ab20282b8513b6ed80dc4319829037b
Fernando Gil <fernando.gil@siemens.com>
2024-08-21 13:58:59 +0200

    Install rbw for fish scripts support

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-sudo apt install -y fzf fish pipx
+sudo apt install -y fzf fish pipx xclip
 
 # install jrnl
 pipx install jrnl
 pipx ensurepath
+
+# install rbw
+curl -s https://git.tozt.net/rbw/releases/deb/rbw_1.12.1_amd64.deb -o /tmp/rbw.deb
+sudo apt install -y /tmp/rbw.deb
 
 stow asdf
 stow fish
